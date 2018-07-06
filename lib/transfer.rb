d5bcd0a68d41f552786cf6342cc9e8f19b6e1009
Emily Wessel <ettl02@gmail.com>
2018-07-06 15:35:52 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,8 +1,10 @@
 class Transfer
 
-  attr_accessor :sender, :receiver 
-  
+  attr_accessor :sender, :receiver
+
   def initialize (sender, receiver, amount)
-    
+    @sender = sender
+    @receiver = receiver 
+    @amount = amount
   end
 end
